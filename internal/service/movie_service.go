56a69faedf6fb0bb249509af4e955040558258eb
Froggkrl <kir07kot@gmail.com>
2025-12-21 22:58:03 +0300

    commit#2

--- internal/service/movie_service.go
+++ internal/service/movie_service.go
@@ -24,3 +24,47 @@ func NewMovieService(repo repository.MovieRepository) MovieService {
 		idGen: utils.NewIDGenerator(),
 	}
 }
+
+func (s *movieService) GetAllMovie() ([]model.Movie, error) {
+	return s.repo.FindAll()
+}
+
+func (s *movieService) GetMovieByID(id string) (model.Movie, error) {
+	return s.repo.FindByID(id)
+}
+
+func (s *movieService) CreateMovie(movie *model.Movie) (*model.Movie, error) {
+	movie.ID = s.idGen.Generate()
+	if movie.Director == nil {
+		movie.Director = &model.Director{
+			FirstName: "",
+			LastName:  "",
+		}
+	}
+	if err := s.repo.Create(movie); err != nil {
+		return nil, err
+	}
+	return movie, nil
+}
+func (s *movieService) UpdateMovie(id string, movie *model.Movie) (*model.Movie, error) {
+	existingMovie, err := s.repo.FindByID(id)
+	if err != nil {
+
+		return nil, err
+	}
+
+	movie.ID = id
+
+	if movie.Director == nil {
+		movie.Director = existingMovie.Director
+	}
+
+	if err := s.repo.Update(id, movie); err != nil {
+		return nil, err
+	}
+	return movie, nil
+}
+
+func (s *movieService) DeleteMovie(id string) error {
+	return s.repo.Delete(id)
+}
